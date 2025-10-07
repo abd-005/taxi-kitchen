aa7aaaa5f14369a3720fcad3ed2918897f0d5e1b
abd-005 <abd.me7@gmail.com>
2025-10-07 22:34:40 +0600

    Orders Cards Added

--- src/components/OrderContainer.jsx
+++ src/components/OrderContainer.jsx
@@ -1,5 +1,6 @@
 import React, { use, } from "react";
 import States from "./States";
+import OrderCard from "./cards/OrderCard";
 
 
 const OrderContainer = ({ promise }) => { 
@@ -9,6 +10,21 @@ const OrderContainer = ({ promise }) => {
   return (
     <div>
       <States></States>
+
+      <section className="w-11/12 mx-auto py-10 grid  grid-cols-1 lg:grid-cols-12 gap-5">
+        <div className="lg:col-span-7">
+          <h2 className="font-bold text-4xl">Currrent Orders</h2>
+
+          <div className="space-y-5">
+            {orders.map((order) => (
+              <OrderCard
+                key={order.id}
+                order={order}
+              ></OrderCard>
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
